eb1b5e28f55606c45be64d7b2774d0b1964629f1
Utkarsh Choudhary <72205579+UtkarshChoudhary19@users.noreply.github.com>
2023-07-01 13:45:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+let promise1 = new Promise((resolve, reject) => {
+    setTimeout(() =>{
+        resolve("Hello, world!");
+    }, Math.floor(Math.random() * 5000) + 1000);
+})
