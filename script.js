bd836d6999162dcb777dd1ad3cb2c8fd1a7f795b
Utkarsh Choudhary <72205579+UtkarshChoudhary19@users.noreply.github.com>
2023-07-01 14:02:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 const myPromise = new Promise(function(myResolve, myReject) {
-  setTimeout(function(){ myResolve("Hello, world!"); }, 5000);
+  setTimeout(function(){ myResolve("Hello, world!"); }, 1000);
 });
 
 myPromise.then(function(value) {
